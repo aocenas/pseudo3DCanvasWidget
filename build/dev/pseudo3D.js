b3975d80ed7272ca06dba867382f359a8aab7045
aocenas <andrej.ocenas.mail@gmail.com>
2011-11-22 00:57:40 +0100

    incorporated last commits

--- build/dev/pseudo3D.js
+++ build/dev/pseudo3D.js
@@ -2,23 +2,19 @@
 /** * almond 0.0.2+ Copyright (c) 2011, The Dojo Foundation All Rights Reserved. * Available via the MIT or new BSD license. * see: http://github.com/jrburke/almond for details *//*jslint strict: false, plusplus: false *//*global setTimeout: false */var requirejs, require, define;(function () {    var defined = {},        aps = [].slice,        req;    if (typeof define === "function") {        //If a define is already in play via another AMD loader,        //do not overwrite.        return;    }    /**     * Given a relative module name, like ./something, normalize it to     * a real name that can be mapped to a path.     * @param {String} name the relative name     * @param {String} baseName a real name that the name arg is relative     * to.     * @returns {String} normalized name     */    function normalize(name, baseName) {        //Adjust any relative paths.        if (name && name.charAt(0) === ".") {            //If have a base name, try to normalize against it,            //otherwise, assume it is a top-level require that will            //be relative to baseUrl in the end.            if (baseName) {                //Convert baseName to array, and lop off the last part,                //so that . matches that "directory" and not name of the baseName's                //module. For instance, baseName of "one/two/three", maps to                //"one/two/three.js", but we want the directory, "one/two" for                //this normalization.                baseName = baseName.split("/");                baseName = baseName.slice(0, baseName.length - 1);                name = baseName.concat(name.split("/"));                //start trimDots                var i, part;                for (i = 0; (part = name[i]); i++) {                    if (part === ".") {                        name.splice(i, 1);                        i -= 1;                    } else if (part === "..") {                        if (i === 1 && (name[2] === '..' || name[0] === '..')) {                            //End of the line. Keep at least one non-dot                            //path segment at the front so it can be mapped                            //correctly to disk. Otherwise, there is likely                            //no path mapping for a path starting with '..'.                            //This can still fail, but catches the most reasonable                            //uses of ..                            break;                        } else if (i > 0) {                            name.splice(i - 1, 2);                            i -= 2;                        }                    }                }                //end trimDots                name = name.join("/");            }        }        return name;    }    function makeRequire(relName, forceSync) {        return function () {            //A version of a require function that passes a moduleName            //value for items that may need to            //look up paths relative to the moduleName            return req.apply(null, aps.call(arguments, 0).concat([relName, forceSync]));        };    }    function makeNormalize(relName) {        return function (name) {            return normalize(name, relName);        };    }    function makeLoad(depName) {        return function (value) {            defined[depName] = value;        };    }    /**     * Makes a name map, normalizing the name, and using a plugin     * for normalization if necessary. Grabs a ref to plugin     * too, as an optimization.     */    function makeMap(name, relName) {        var prefix, plugin,            index = name.indexOf('!');        if (index !== -1) {            prefix = normalize(name.slice(0, index), relName);            name = name.slice(index + 1);            plugin = defined[prefix];            //Normalize according            if (plugin && plugin.normalize) {                name = plugin.normalize(name, makeNormalize(relName));            } else {                name = normalize(name, relName);            }        } else {            name = normalize(name, relName);        }        //Using ridiculous property names for space reasons        return {            f: prefix ? prefix + '!' + name : name, //fullName            n: name,            p: plugin        };    }    function main(name, deps, callback, relName) {        var args = [],            usingExports,            cjsModule, depName, i, ret, map;        //Use name if no relName        if (!relName) {            relName = name;        }        //Call the callback to define the module, if necessary.        if (typeof callback === 'function') {            //Pull out the defined dependencies and pass the ordered            //values to the callback.            if (deps) {                for (i = 0; i < deps.length; i++) {                    map = makeMap(deps[i], relName);                    depName = map.f;                    //Fast path CommonJS standard dependencies.                    if (depName === "require") {                        args[i] = makeRequire(name);                    } else if (depName === "exports") {                        //CommonJS module spec 1.1                        args[i] = defined[name] = {};                        usingExports = true;                    } else if (depName === "module") {                        //CommonJS module spec 1.1                        cjsModule = args[i] = {                            id: name,                            uri: '',                            exports: defined[name]                        };                    } else if (depName in defined) {                        args[i] = defined[depName];                    } else if (map.p) {                        map.p.load(map.n, makeRequire(relName, true), makeLoad(depName), {});                        args[i] = defined[depName];                    }                }            }            ret = callback.apply(defined[name], args);            if (name) {                //If setting exports via "module" is in play,                //favor that over return value and exports. After that,                //favor a non-undefined return value over exports use.                if (cjsModule && cjsModule.exports !== undefined) {                    defined[name] = cjsModule.exports;                } else if (!usingExports) {                    //Use the return value from the function.                    defined[name] = ret;                }            }        } else if (name) {            //May just be an object definition for the module. Only            //worry about defining if have a module name.            defined[name] = callback;        }    }    requirejs = req = function (deps, callback, relName, forceSync) {        if (typeof deps === "string") {            //Just return the module wanted. In this scenario, the            //deps arg is the module name, and second arg (if passed)            //is just the relName.            //Normalize module name, if it contains . or ..            return defined[makeMap(deps, callback).f];        } else if (!deps.splice) {            //deps is a config object, not an array.            //Drop the config stuff on the ground.            if (callback.splice) {                //callback is an array, which means it is a dependency list.                //Adjust args if there are dependencies                deps = callback;                callback = arguments[2];            } else {                deps = [];            }        }        //Simulate async callback;        if (forceSync) {            main(null, deps, callback, relName);        } else {            setTimeout(function () {                main(null, deps, callback, relName);            }, 15);        }        return req;    };    /**     * Just drops the config on the floor, but returns req in case     * the config return value is used.     */    req.config = function () {        return req;    };    /**     * Export require as a global, but only if it does not already exist.     */    if (!require) {        require = req;    }    define = function (name, deps, callback) {        //This module may not have dependencies        if (!deps.splice) {            //deps is not an array, so probably means            //an object literal for the value. Adjust args.            callback = deps;            deps = [];        }        main(name, deps, callback);    };    define.amd = {        jQuery: true    };}());
 define("../lib/almond", function(){});
 
-
-
 define('console',[],function (){
 	
 	if (!window.console) console = {};
 	console.log = console.log || function(){};
-	  
+  
 	// Comment this line if you want console logs.
-	console.log = function(){};
+	//console.log = function(){};
 	  
 	return console;
 });
-
 /*
  * Point object do define some point in space with coordinates and diameter.
  */
-
 define('Point',['console'],function (console){
 	
 	/*
@@ -40,49 +36,53 @@ define('Point',['console'],function (console){
 		 */
 		rotate : function (xyAngle,xzAngle,yzAngle,centerPoint){
 			
-			
 			console.log("rotating point x,y,z = " + this.x + "," + this.y + "," + this.z);
 			console.log("angle xy,xz,yz " + xyAngle/Math.PI + "PI," + xzAngle/Math.PI + "PI," + yzAngle/Math.PI + "PI, around x,y,z = " 
 							+ centerPoint.x + "," + centerPoint.y + "," + centerPoint.z);
 			
+			var self = this;
+			[[xyAngle,'x','y'],[xzAngle,'x','z'],[yzAngle,'y','z']].forEach(function(angle){
 			
-			// Calculate coordinates relative to the point we are going rotate around.
-			var centerRelativeX = this.x - centerPoint.x;
-			var centerRelativeY = this.y - centerPoint.y;
-			var centerRelativeZ = this.z - centerPoint.z;
-			
-			
-			// Calculating distance from center but only on the XZ plane.
-			var distanceFromCenterXZ = Math.round(
-									  Math.sqrt(Math.pow(centerRelativeZ,2) +
-												Math.pow(centerRelativeX,2)
-												)
-									  );
-		
-			// To correctly compute the angle we need to know into which quadrant are we going to fall.
-			var quadrant = centerRelativeX >= 0 ? (centerRelativeZ >= 0? 1 : 2) : (centerRelativeZ >= 0 ? 4 : 3);
+				// Calculate coordinates relative to the point, we are going rotate around.
+				var relative = {x : self.x - centerPoint.x,
+												y : self.y - centerPoint.y,
+												z : self.z - centerPoint.z};
+				
+				console.log("rotating in plane " + angle[1] + angle[2] + " by " + angle[0]);
+        
+        // Calculating distance from center.
+        var distanceFromCenter = Math.round(
+          Math.sqrt(Math.pow(relative[angle[2]],2) +
+            Math.pow(relative[angle[1]],2)
+          )
+        );
+        
+        // To correctly compute the angle we need to know into which quadrant are we going to fall.
+        var quadrant = relative[angle[1]] >= 0 ? (relative[angle[2]] >= 0? 1 : 2) : (relative[angle[2]] >= 0 ? 4 : 3);
 
-			var angleFromCenter = distanceFromCenterXZ === 0 ? 0 : Math.asin(Math.abs(centerRelativeX)/distanceFromCenterXZ);
+        var angleFromCenter = distanceFromCenter === 0 ? 0 : Math.asin(Math.abs(relative[angle[1]])/distanceFromCenter);
 
-			switch(quadrant){
-				case 1: break;
-				case 2:	angleFromCenter = Math.PI - angleFromCenter;
-						break;
-				case 3:	angleFromCenter = Math.PI + angleFromCenter;
-						break;
-				case 4:	angleFromCenter = Math.PI*2 - angleFromCenter;
-						break;
-			}
-			
-			console.log("angleFromCenter = " + angleFromCenter/Math.PI + "PI");
-			
-			// Adding angle to x,y,z (we are turning it only on the horizontal XZ plane 
-			// so we need to compute only x and z coordinates)
-			this.x = (Math.sin(xzAngle + angleFromCenter) * distanceFromCenterXZ) + centerPoint.x;
-			this.z = (Math.cos(xzAngle + angleFromCenter) * distanceFromCenterXZ) + centerPoint.z;
-			
-			console.log("after rotation x,y,z = " + this.x + "," + this.y + "," + this.z);
-			
+        switch(quadrant){
+          case 1: break;
+          case 2:	angleFromCenter = Math.PI - angleFromCenter;
+              break;
+          case 3:	angleFromCenter = Math.PI + angleFromCenter;
+              break;
+          case 4:	angleFromCenter = Math.PI*2 - angleFromCenter;
+              break;
+        }
+        console.log("angleFromCenter = " + angleFromCenter/Math.PI + "PI");
+        
+        // Adding angle to x,y,z
+				
+				var first = Math.round(((Math.sin(angle[0] + angleFromCenter) * distanceFromCenter) + centerPoint[angle[1]])*1000)/1000;
+				var second = Math.round(((Math.cos(angle[0] + angleFromCenter) * distanceFromCenter) + centerPoint[angle[2]])*1000)/1000;
+        self[angle[1]] = first;
+        self[angle[2]] = second;
+        
+        console.log("after rotation x,y,z = " + self.x + "," + self.y + "," + self.z);
+        
+      });
 		}
 	};
 	
@@ -174,32 +174,32 @@ define('Context3D',['console','globals'],function (console, globals){
 				this.context.arc(posX,posY,radius,0,Math.PI*2,false);
 			}
 		},
-	fill : function(){
-	  this.context.fill();
-	},
-	stroke : function(){
-	  this.context.stroke();
-	},
-	beginPath : function(){
-	  this.context.beginPath();
-	},
-	/*
-	 * Drawing each point of the grid separately. It takes grid of points and function which 
-	 * will be caled after the drawing(that means fill or stroke method)
-	 */
-	drawPointGrid : function (pointGrid,func){
-		this.beginPath();
-		pointGrid.points.forEach(function(point){this.drawPoint(point)},this);
-		try{
-			func.apply(this);
-		}catch( e){
-			// Fail over.
-			this.stroke();
-		}
-	},
-	clear : function (){
-		this.canvas.width = this.canvas.width;
-	}
+    fill : function(){
+      this.context.fill();
+    },
+    stroke : function(){
+      this.context.stroke();
+    },
+    beginPath : function(){
+      this.context.beginPath();
+    },
+    /*
+     * Drawing each point of the grid separately. It takes grid of points and function which 
+     * will be caled after the drawing(that means fill or stroke method)
+     */
+    drawPointGrid : function (pointGrid,func){
+      this.beginPath();
+      pointGrid.points.forEach(function(point){this.drawPoint(point)},this);
+      try{
+        func.apply(this);
+      }catch( e){
+        // Fail over.
+        this.stroke();
+      }
+    },
+    clear : function (){
+      this.canvas.width = this.canvas.width;
+    }
   };
   
   return Context3D;
@@ -224,27 +224,25 @@ define('PointGrid',['console', 'Point', 'globals','Color'],function (console, Po
 		
 		for(var i = 0,lengthi = mapGrid.length; i < lengthi; i++){ //for Z layers or depth
 		  for(var j = 0,lengthj = mapGrid[i].length; j < lengthj; j++){ //for Y lines or height
-			for(var k = 0, lengthk = mapGrid[i][j].length; k < lengthk; k++){ //for X columns or width
-			  
-			  if(mapGrid[i][j][k] == 1){
-				
-				
-				// Object defined by the mapGrid array should be in the center of the canvas
-				// so some we need to calculate right coordinates
-				
-				var relativeXGrid = k - Math.floor(lengthk/2);
-				var relativeYGrid = j - Math.floor(lengthj/2);
-				var relativeZGrid = i - Math.floor(lengthi/2);
-				  
-				var x = (relativeXGrid * globals.GRID_SPACING) + globals.X00;
-				var y = (relativeYGrid * globals.GRID_SPACING) + globals.Y00;
-				var z = (relativeZGrid * globals.GRID_SPACING) + globals.Z00;
-				
-				this.points[this.points.length] = new Point(x,y,z,globals.POINT_RADIUS,new Color(0,0,0));
-				
-			  }
-			  
-			}
+        for(var k = 0, lengthk = mapGrid[i][j].length; k < lengthk; k++){ //for X columns or width
+          
+          if(mapGrid[i][j][k] == 1){
+          
+            // Object defined by the mapGrid array should be in the center of the canvas
+            // so we need to calculate right coordinates
+            
+            var relativeXGrid = k - Math.floor(lengthk/2);
+            var relativeYGrid = j - Math.floor(lengthj/2);
+            var relativeZGrid = i - Math.floor(lengthi/2);
+
+            var x = (relativeXGrid * globals.GRID_SPACING) + globals.X00;
+            var y = (relativeYGrid * globals.GRID_SPACING) + globals.Y00;
+            var z = (relativeZGrid * globals.GRID_SPACING) + globals.Z00;
+            
+            this.points[this.points.length] = new Point(x,y,z,globals.POINT_RADIUS,new Color(0,0,0));
+          
+          }
+        }
 		  }
 		}
 	};
@@ -257,12 +255,10 @@ define('PointGrid',['console', 'Point', 'globals','Color'],function (console, Po
 					point.rotate(xyAngle,xzAngle,yzAngle,centerPoint)
 				});
 			
-			
 		}
 	};
 	
 	return PointGrid;
-	
 });
 define('Pseudo3D',['Context3D','PointGrid','globals'],function(Context3D,PointGrid,globals){
 
@@ -277,7 +273,7 @@ define('Pseudo3D',['Context3D','PointGrid','globals'],function(Context3D,PointGr
 			var rotate = function (){
 				c3D.clear();
 				c3D.drawPointGrid(grid,c3D.fill);
-				grid.rotate(0,options.oneStepAngle,0,{x:globals.X00 + options.centerXOffset,
+				grid.rotate(0,0,options.oneStepAngle,{x:globals.X00 + options.centerXOffset,
 													  y:globals.Y00,
 													  z:globals.Z00 + options.centerZOffset});
 				angle = angle + options.oneStepAngle;
