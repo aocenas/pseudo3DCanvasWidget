e6c5391daa76287cce68cb469450e587520c30a8
aocenas <andrej.ocenas.mail@gmail.com>
2011-12-04 12:59:13 +0100

    Added new tests for point rotation.

--- tests/testPoint.js
+++ tests/testPoint.js
@@ -18,32 +18,48 @@ $(document).ready(function(){
 			var point = new Point(0, 0, 0, 10, new Color(255,255,255));
 			var pi = Math.PI;
 			point.rotate(pi/2, 0, 0, {x : 0, y : 0, z : 0});
-			equal(point.toString(),"x0y0z0", "Rotation 1 ok");
+			equal(point.toString(),"x0y0z0", "Rotation 1");
 			
 			point = new Point(0, 0, 0, 10, new Color(255,255,255));
 			point.rotate(0, pi, 0, {x : 10, y : 0, z : 0});
-			equal(point.toString(),"x20y0z0", "Rotation 2 ok");
+			equal(point.toString(),"x20y0z0", "Rotation 2");
 			
 			point = new Point(0, 0, 0, 10, new Color(255,255,255));
 			point.rotate(0, 0, pi, {x : 0, y : 0, z : 10});
-			equal(point.toString(),"x0y0z20", "Rotation 3 ok");
+			equal(point.toString(),"x0y0z20", "Rotation 3");
 			
 			point = new Point(0, 0, 0, 10, new Color(255,255,255));
 			point.rotate(pi, 0, 0, {x : 0, y : 10, z : 0});
-			equal(point.toString(),"x0y20z0", "Rotation 4 ok");
+			equal(point.toString(),"x0y20z0", "Rotation 4");
 			
 			point = new Point(0, 0, 0, 10, new Color(255,255,255));
 			point.rotate(0, pi/2, pi, {x : 10, y : 0, z : 0});
-			equal(point.toString(),"x10y0z-10", "Rotation 5 ok");
-			
-			point.rotate(pi/2, 0, 0, {x : 20, y : 0, z : 0});
-			equal(point.toString(),"x20y10z-10", "Rotation 6 ok");
+			var point2 = new Point(0, 0, 0, 10, new Color(255,255,255));
+			point2.rotate(0, pi/2, 0, {x : 10, y : 0, z : 0});
+			point2.rotate(0, 0, pi, {x : 10, y : 0, z : 0});
+			equal(point.toString(),point2.toString(), "Rotation 5");
 			
 			point = new Point(10, 10, 0, 10, new Color(255,255,255));
 			point.rotate(pi/2, pi/2, 0, {x : 0, y : 0, z : 0});
-			equal(point.toString(),"x0y10z-10", "Rotation 7 ok");
+			point2 = new Point(10, 10, 0, 10, new Color(255,255,255));
+			point2.rotate(pi/2, 0, 0, {x : 0, y : 0, z : 0});
+			point2.rotate(0, pi/2, 0, {x : 0, y : 0, z : 0});
+			equal(point.toString(),point2.toString(), "Rotation 6");
 			
+			point = new Point(10, 10, 10, 10, new Color(255,255,255));
+			point.rotate(pi/2, 0, pi, {x : 0, y : 0, z : 0});
+			point2 = new Point(10, 10, 10, 10, new Color(255,255,255));
+			point2.rotate(pi/2, 0, 0, {x : 0, y : 0, z : 0});
+			point2.rotate(0, 0, pi, {x : 0, y : 0, z : 0});
+			equal(point.toString(),point2.toString(), "Rotation 7	");
 			
+			point = new Point(10, 10, 10, 10, new Color(255,255,255));
+			point.rotate(pi/2, pi/2, pi, {x : 0, y : 0, z : 0});
+			point2 = new Point(10, 10, 10, 10, new Color(255,255,255));
+			point2.rotate(pi/2, 0, 0, {x : 0, y : 0, z : 0});
+			point2.rotate(0, pi/2, 0, {x : 0, y : 0, z : 0});
+			point2.rotate(0, 0, pi, {x : 0, y : 0, z : 0});
+			equal(point.toString(),point2.toString(), "Rotation 8	");
 		});
 	});
 });
