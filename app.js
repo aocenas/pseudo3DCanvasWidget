286cdf9321c9e0365dc3cca627c88d9a3d3d9302
aocenas <andrej.ocenas.mail@gmail.com>
2011-12-04 21:05:06 +0100

    updated example

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-
-
-
 window.onload = function () {
 	
-	Pseudo3D("main_canvas").addModel([[[1,1,1]]])
+	var canvas = Pseudo3D("main_canvas");
+	canvas.addModel([[[1,1,1]]])
 		.rotateXY(Math.PI/2,{x : 200, y : 100 , z : 200})
-		.addModel([[[1,0,0]]]).draw();
-			
+		.addModel([[[1,0,0,0,0]]]).draw();
+	 
+	var count = 0;
+	(function rotate(){
+		canvas.rotateXZ(Math.PI/258,{x : 200, y : 100 , z : 200});
+		if(count < 512){
+			setTimeout(rotate,1);
+		}
+		count = count + 1;
+	})();
+
+	
 };
