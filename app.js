aa7d4502ecf033f7b7ba60b753b6a7dfa72cdf6f
aocenas <andrej.ocenas.mail@gmail.com>
2011-12-05 23:31:47 +0100

    added animate example

--- app.js
+++ app.js
@@ -4,15 +4,9 @@ window.onload = function () {
 	canvas.addModel([[[1,1,1]]])
 		.rotateXY(Math.PI/2,{x : 200, y : 100 , z : 200})
 		.addModel([[[1,0,0,0,0]]]).draw();
-	 
-	var count = 0;
-	(function rotate(){
-		canvas.rotateXZ(Math.PI/258,{x : 200, y : 100 , z : 200});
-		if(count < 512){
-			setTimeout(rotate,1);
-		}
-		count = count + 1;
-	})();
-
 	
+	canvas.animate(function(){
+		this.rotateXZ(Math.PI/64,{x : 200, y : 100 , z : 200});
+		this.rotateXZ(Math.PI/64,{x : 200, y : 100 , z : 100});
+	},64,0.1);
 };
